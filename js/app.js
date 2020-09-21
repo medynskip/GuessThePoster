4b6f0e93831f9b58205ea95dc2dc4affb6fe4082
Piotr Medynski <medynski@outlook.com>
2020-09-21 18:36:55 +0200

    point system and new fetch script

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const pw = 600;
 const ph = 900;
-const box = 12;
+const box = 32;
 let answerString = "";
 let response = "";
 let points = 0;
+const movies = [];
 
 const canvas = document.getElementById('poster');
 const calculate = document.getElementById('calculate');
@@ -13,8 +14,11 @@ const img = new Image;
 const scratch = new Image;
 
 const answer = document.getElementById("answer");
-const scoreCurrent = document.getElementById('score-current')
-const scoreTotal = document.getElementById('score-total')
+const scoreCurrent = document.getElementById('score-current');
+const scoreTotal = document.getElementById('score-total');
+const submitBtn = document.getElementById("submit");
+const skipBtn = document.getElementById("skip");
+const revealBtn = document.getElementById("reveal");
 
 const allowed = `ABCDEFGHIJKLMNOPQRSTUWVXYZabcdefghijklmnopqrstuwvxyz0123456789`;
 
@@ -55,23 +59,17 @@ const countPoints = (canvas) => {
         colorData.data[i] > 0 ? pointCount += 1 : pointCount += 0;
     }
     points = Math.round(1000 * (pointCount / (ph * pw)));
-    console.log(points);
     scoreCurrent.innerText = 1000 - points;
 }
 
 const posterReveal = (movies, item) => {
-    // const canvas = document.getElementById('poster');
-    // const calculate = document.getElementById('calculate');
-    // const ctx = canvas.getContext('2d');
-    // const ctxCalc = calculate.getContext('2d');
-    // const img = new Image;
-    // const scratch = new Image;
+    submitBtn.disabled = false;
+    revealBtn.disabled = false;
 
     scratch.onload = () => {
         ctx.drawImage(scratch, 0, 0);
         ctxCalc.clearRect(0, 0, calculate.width, calculate.height)
     }
-
 
     const strDataURI = `https://image.tmdb.org/t/p/w${pw}_and_h${ph}_bestv2${movies[item].poster_path}`;
     img.src = strDataURI;
@@ -84,68 +82,53 @@ const posterReveal = (movies, item) => {
         canvas.addEventListener("mousedown", (e) => {
             draw = true;
             drawCanvas([ctx, ctxCalc], e.offsetX, e.offsetY);
-            // drawCanvas(ctxCalc, e.offsetX, e.offsetY);
+            countPoints(ctxCalc);
         })
 
         canvas.addEventListener("mousemove", (e) => {
             if (draw == true) {
                 drawCanvas([ctx, ctxCalc], e.offsetX, e.offsetY);
-                // drawCanvas(ctxCalc, e.offsetX, e.offsetY);
+                countPoints(ctxCalc);
             }
         })
         document.addEventListener("mouseup", (e) => {
             draw = false;
-            countPoints(ctxCalc);
         })
     };
-
-    const revealBtn = document.getElementById("reveal");
-    revealBtn.addEventListener("click", () => {
-        ctx.drawImage(img, 0, 0)
-    })
-
-
 }
-
 
 const newMovie = () => {
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=a7a279b94f7be340c3155d4b7df30384&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${rand(1, 10)}&with_genres=16`;
-    // const url = `https://api.themoviedb.org/3/discover/movie?api_key=a7a279b94f7be340c3155d4b7df30384&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${rand(1, 10)}`;
-    // const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=a7a279b94f7be340c3155d4b7df30384&language=en-US&page=${rand(1, 10)}`;
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((myJson) => {
-            let movies = myJson.results;
-            let item = rand(0, 19);
-            // document.getElementById("text").innerText = movies[item].title;
-            answer.innerHTML = "";
-            answerString = "";
-            for (let i = 0; i < movies[item].title.length; i++) {
-                if (movies[item].title[i] == " ") {
-                    answer.innerHTML += `<span> </span></br>`;
-                } else if (allowed.indexOf(movies[item].title[i]) == -1) {
-                    answer.innerHTML += `<span> ${movies[item].title[i]} </span>`
-                } else {
-                    answerString += movies[item].title[i];
-                    answer.innerHTML += `<input type="text" name="${i}" required minlength="1" maxlength="1" size="1">`;
-                }
-            }
-            fieldFocus();
-            posterReveal(movies, item);
-
-        });
+    let item = rand(0, movies.length);
+    answer.innerHTML = "";
+    answerString = "";
+    for (let i = 0; i < movies[item].title.length; i++) {
+        if (movies[item].title[i] == " ") {
+            answer.innerHTML += `<span> </span></br>`;
+        } else if (allowed.indexOf(movies[item].title[i]) == -1) {
+            answer.innerHTML += `<span> ${movies[item].title[i]} </span>`
+        } else {
+            answerString += movies[item].title[i];
+            answer.innerHTML += `<input type="text" name="${i}" required minlength="1" maxlength="1" size="1">`;
+        }
+    }
+    fieldFocus();
+    posterReveal(movies, item);
 }
 
-const skipBtn = document.getElementById("skip");
+
 skipBtn.addEventListener("click", () => {
     document.getElementById('text').innerText = ""
     newMovie();
     scoreCurrent.innerText = 1000;
 })
 
-const submitBtn = document.getElementById("submit");
+revealBtn.addEventListener("click", () => {
+    ctx.drawImage(img, 0, 0);
+    submitBtn.disabled = true;
+    revealBtn.disabled = true;
+})
+
+
 submitBtn.addEventListener("click", () => {
     const fields = document.querySelectorAll('input');
     response = "";
@@ -158,9 +141,32 @@ submitBtn.addEventListener("click", () => {
         let total = 0;
         total = parseInt(scoreTotal.innerText) + parseInt(scoreCurrent.innerText);
         scoreTotal.innerText = total;
+        submitBtn.disabled = true;
+        revealBtn.disabled = true;
     } else {
         document.getElementById('text').innerText = "ŹLE"
     }
 })
 
-newMovie();
+
+const getRecords = () => {
+    const urls = [];
+    const url2 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a279b94f7be340c3155d4b7df30384&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=16&page=`;
+    for (let i = 1; i <= 10; i++) {
+        urls.push(fetch(url2 + i));
+    }
+    Promise.all(urls)
+        .then(function (responses) {
+            return Promise.all(responses.map(function (response) {
+                return response.json();
+            }));
+        }).then(function (data) {
+            for (let x = 0; x < data.length; x++) {
+                movies.push(...data[x].results)
+            }
+            newMovie();
+        })
+
+}
+
+getRecords();
