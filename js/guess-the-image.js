8c4add1ca3cff481a09662d00516d3d9e6ffb815
Piotr Medynski <medynski@outlook.com>
2020-12-07 20:02:56 +0100

    working inputs and selectors

--- js/guess-the-image.js
+++ js/guess-the-image.js
@@ -25,7 +25,6 @@ let currentPoints = 0;
 let totalPoints = 0;
 let slide = 0;
 let draw = false;
-// let loading = false;
 const movies = [...data];
 
 const answer = document.getElementById("answer");
@@ -56,13 +55,13 @@ const unloading = () => {
 
 const checkButton = (key, position, fields) => {
     if (key == 'Backspace' && position > 0) {
-        fields[position - 1].focus();
+        fields[position - 1].select();
     } else if (key == 'Backspace' && position == 0) {
         fields[position].focus();
     } else if (position == fields.length - 1) {
         document.getElementById('submit').focus();
     } else {
-        fields[position + 1].focus();
+        fields[position + 1].select();
     }
 }
 
@@ -127,13 +126,9 @@ const initiateCanvases = (currentImage) => {
 }
 
 const generateImage = (file_path) => {
-
     const strDataURI = `https://image.tmdb.org/t/p/w1280${file_path}`;
-    console.log(strDataURI);
-    // const strDataURI = `http://image.tmdb.org/t/p/w1920_and_h800_multi_faces${file_path}`;
     img.src = strDataURI;
     scratch.src = "../images/scratch.jpg";
-
     img.onload = () => {
         unloading();
         initiateCanvases(img)
@@ -148,47 +143,39 @@ const fetchImage = (id) => {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            const index = rand(0, data.backdrops.length)
+            const index = rand(0, data.backdrops.length - 1)
             const imgPath = data.backdrops[index].file_path;
             generateImage(imgPath);
         })
 }
 
 const posterReveal = (item) => {
+    nextBtn.disabled = false;
     submitBtn.disabled = false;
     revealBtn.disabled = false;
-
     scratch.onload = () => {
         ctx.drawImage(scratch, 0, 0, pw, ph);
         ctxCalc.clearRect(0, 0, calculate.width, calculate.height)
     }
-
     fetchImage(movies[item].id);
-
-    // const strDataURI = `http://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${movies[item].img}`;
-    // img.src = strDataURI;
-    // scratch.src = "../images/scratch.jpg";
-
-    // img.onload = () => {
-    //     unloading();
-    //     initiateCanvases(img)
-    //     canvas.addEventListener("mousedown", mouseDownHandle);
-    //     canvas.addEventListener("mousemove", mouseMoveHandle);
-    //     canvas.addEventListener("mouseup", mouseUpHandle);
-    // };
 }
 
 const createInputs = (item) => {
-    for (let i = 0; i < movies[item].title.length; i++) {
-        if (movies[item].title[i] == " ") {
-            answer.innerHTML += `<span> </span></br>`;
-        } else if (allowed.indexOf(movies[item].title[i]) == -1) {
-            answer.innerHTML += `<span> ${movies[item].title[i]} </span>`
-        } else {
-            answerString += movies[item].title[i];
-            answer.innerHTML += `<input type="text" name="${i}" required minlength="1" maxlength="1" size="1">`;
+    const words = movies[item].title.split(" ");
+    let generated = ``;
+    for (let i = 0; i < words.length; i++) {
+        generated += `<span>`;
+        for (let j = 0; j < words[i].length; j++) {
+            if (allowed.indexOf(words[i][j]) == -1) {
+                generated += `<span> ${(words[i][j])} </span>`
+            } else {
+                answerString += words[i][j];
+                generated += `<input type="text" name="${i,j}" required minlength="1" maxlength="1" size="1">`;
+            }
         }
+        generated += `</span>`;
     }
+    answer.innerHTML = generated;
 }
 
 const newMovie = () => {
@@ -199,7 +186,6 @@ const newMovie = () => {
     createInputs(index);
     fieldFocus();
 }
-
 
 nextBtn.addEventListener("click", () => {
     clearMouseHandlers();
@@ -242,49 +228,21 @@ submitBtn.addEventListener("click", () => {
 newBtn.addEventListener("click", () => {
     slide = 0;
     totalPoints = 0;
+    scoreTotal.innerText = totalPoints;
     getRecords();
 })
 
 const getRecords = () => {
-    // const urls = [];
-    // const url2 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a279b94f7be340c3155d4b7df30384&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&vote_count.gte=5000&page=`;
-    // const url2 = `https://api.themoviedb.org/3/discover/movie?api_key=a7a279b94f7be340c3155d4b7df30384&language=en-US&sort_by=popularity.desc&include_adult=false&with_genres=16&include_video=false&vote_count.gte=5000&page=`;
-    // for (let i = 1; i <= 8; i++) {
-    //     urls.push(fetch(url2 + i));
-    // }
-    // Promise.all(urls)
-    //     .then(function (responses) {
-    //         return Promise.all(responses.map(function (response) {
-    //             return response.json();
-    //         }));
-    //     }).then(function (data) {
-    //         for (let x = 0; x < data.length; x++) {
-    //             movies.push(...data[x].results)
-    //         }
-    //         newMovie();
-    //         console.log(movies);
-    //         let htmlCode = '';
-    //         movies.map((el) => {
-    //             // var text = document.createTextNode("Tutorix is the best e-learning platform");
-    //             // tag.appendChild(text);
-    //             htmlCode += `{<br />
-    //                     id: ${el.id},<br / >
-    //                     img: '${el.backdrop_path}',<br / >
-    //                     title: '${el.title}'<br / >
-    //                     },`
-    //         });
-    //         var tag = document.createElement("p");
-    //         tag.innerHTML = htmlCode;
-    //         document.getElementById("json").appendChild(tag);
-    //     })
-    // console.log(movies);
     slide += 1;
     if (slide <= 10) {
         loading();
         document.getElementById("slide-number").innerText = slide;
         newMovie();
-        slide == 10 ? nextBtn.innerText = "Summary" : null;
+        slide == 10 ? nextBtn.innerText = "Summary" : nextBtn.innerText = "Next";
     } else {
+        nextBtn.disabled = true;
+        submitBtn.disabled = true;
+        revealBtn.disabled = true;
         alert(`Gratulacje, zdobyles ${totalPoints} punktów`)
     }
 }
